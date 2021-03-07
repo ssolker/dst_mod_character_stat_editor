5fef12e238008698fcb4c28dae0554c29c185d83
shuaib <ssolker@gmail.com>
2021-03-07 14:55:40 -0500

    Added numeric spinner and movement modifier

--- scripts/components/modcharacterstats.lua
+++ scripts/components/modcharacterstats.lua
@@ -13,6 +13,18 @@ function ModCharacterStats:GetComponents()
 	print("GET COMPONENTS")
 end
 
+function ModCharacterStats:UpdateCharacter(inst, data, x)
+	print("REACHED")
+	print(inst)
+	if (type(data) == "table") then
+		for k,v in pairs(data) do
+			print(k,v)
+		end
+	else
+		print(data)
+	end
+end
+
 function ModCharacterStats:Loaded()
 	-- self.inst.components.hunger.current = self.inst.components.hunger.max
 	-- self.inst.components.health.current = self.inst.components.health.maxhealth
@@ -25,23 +37,6 @@ function ModCharacterStats:Print()
 	for k,v in pairs(self.inst.components.modcharacterstats.inst) do
 		print(k,v)
 	end
-end
-
-function ModCharacterStats:Another()
-	if (self.inst.components.health) then
-		print(self.inst.components.health.current)
-		self.inst.components.health.current = self.inst.components.health.maxhealth
-	else
-		print("NOT HEALTH")
-	end
-	for k,v in pairs(self.inst.components) do
-		print(k,v)
-	end
-end
-
-function ModCharacterStats:PrintHealth(inst)
-	print("TEST HEALTH PRINT")
-	self:Another()
 end
 
 function ModCharacterStats:PrintStatus(inst)
@@ -58,8 +53,24 @@ function ModCharacterStats:FillStatus(inst)
 	inst.components.sanity.current = inst.components.sanity.max
 end
 
+-- locomotor
+function ModCharacterStats:SetRunSpeed(inst, value)
+	inst.components.locomotor.runspeed = value
+end
+
+function ModCharacterStats:SetWalkSpeed(inst, value)
+	inst.components.locomotor.walkspeed = value
+end
+
+--
 function ModCharacterStats:FillHealth(inst) 
-	inst.components.health.current = inst.components.health.maxhealth
+	inst.components.health:SetPenalty(0)
+	inst.components.health:SetMaxHealth(inst.components.health.maxhealth)
+	-- print(inst.components.health:GetPenaltyPercent())
+	-- print(inst.components.health:GetPercent())
+	-- print(inst.components.health.maxhealth)p
+	-- print(inst.components.health)
+	-- print(inst.replica.health)
 end
 
 function ModCharacterStats:FillHunger(inst) 
